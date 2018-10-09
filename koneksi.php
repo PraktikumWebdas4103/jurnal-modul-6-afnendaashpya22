09805032ad1ecee59804f842d9da0cec31f06779
afnendaashpya22 <43516021+afnendaashpya22@users.noreply.github.com>
2018-10-09 16:59:19 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,3 @@
+<?php
+	$koneksi = mysqli_connect('localhost','root','','mydata') or die (mysqli_error());
+?>
---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+	include 'home.php';
+?>
+
+<form action="" method="POST">
+	<center>
+		<h1>LOGIN</h1>
+		<table>
+			<tr>
+				<td>Username</td>
+				<td>:</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td>:</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td><input type="submit" name="submit" value="LOGIN"></td>
+			</tr>
+		</table>
+	</center>
+</form>
+
+<?php
+	session_start()
+	session_register('username');
+	session_register('password');
+
+	$_SESSION['username']=$username;
+	header("location:home.php")
+?>
